12eeed6a33e498c7bff26863a04497e1008a41c1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to: Implement a custom colorizer
+
+
+This example demonstrates how to implement a custom colorizer that can be used to color a TreeMap.
+
+
+<h3>Description</h3>
+
+<p>To do this, design a class implementing the&nbsp;<strong>ITreeMapColorizer</strong>&nbsp;interface and implement the interface&nbsp;<strong>GetItemColor&nbsp;</strong>method. After that, an instance of the class can be assigned to the&nbsp;<strong>TreeMapControl.Colorizer</strong>&nbsp;property.</p>
+
+<br/>
+
+
---
+++ VB/CustomColorizerSample/CustomColorizer.vb
@@ -0,0 +1,52 @@
+﻿#Region "#CustomColorizerImplementation"
+Imports DevExpress.TreeMap
+Imports DevExpress.XtraTreeMap
+Imports System
+Imports System.Drawing
+
+Namespace CustomColorizerSample
+    Friend Class CustomColorizer
+        Implements ITreeMapColorizer
+
+
+        Private palette_Renamed As Palette = Palette.Office2016Palette
+
+        Public Property Palette() As Palette
+            Get
+                Return palette_Renamed
+            End Get
+            Set(ByVal value As Palette)
+                If palette_Renamed.Equals(value) Then
+                    Return
+                End If
+                palette_Renamed = value
+                RaiseColorizerChanged()
+            End Set
+        End Property
+
+        Public Event ColorizerChanged As ColorizerChangedEventHandler Implements ITreeMapColorizer.ColorizerChanged
+
+        Public Function GetItemColor(ByVal item As ITreeMapItem, ByVal group As TreeMapItemGroupInfo) As Color Implements ITreeMapColorizer.GetItemColor
+            If item.Children.Count = 0 Then
+                Dim itemColor As Color = Palette(group.ItemIndex Mod Palette.Count)
+                Dim itemWeight As Double = (item.Value - group.MinValue) / (group.MaxValue - group.MinValue)
+                If Double.IsNaN(itemWeight) Then
+                    itemWeight = 1
+                End If
+
+                Return Color.FromArgb(CInt((itemWeight * 255)), itemColor.R, itemColor.G, itemColor.B)
+            Else
+                Return Palette(Palette.Count - 1 - (group.GroupIndex + group.GroupLevel + group.ItemIndex) Mod Palette.Count)
+            End If
+
+        End Function
+
+        Private Sub RaiseColorizerChanged()
+            If ColorizerChangedEvent Is Nothing Then
+                Return
+            End If
+            ColorizerChangedEvent.Invoke(Me, New ColorizerChangedEventArgs())
+        End Sub
+    End Class
+End Namespace
+#End Region ' #CustomColorizerImplementation
